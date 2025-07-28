3fbaa62d6f5c0ad20536950f765b51c067bf42d8
Eduardo Furlaneti <eduardohppa@gmail.com>
2025-07-28 11:22:42 -0300

    Aprendendo métodos estáticos

--- ex1/app/classes/UploadFoto.php
+++ ex1/app/classes/UploadFoto.php
@@ -7,6 +7,14 @@ class UploadFoto extends Upload
     public $file;
     public $newName;
     public $extensions = ['png', 'jpg'];
+    
+    const PROPRIEDADE_TESTE = 'Oi';
+    public static $PROPRIEDED_TESTE = 'Oi';
+
+    public static function TESTE($nome)
+    {
+        echo "Oi $nome" . PHP_EOL;
+    }
 
     public function __construct($file)
     {
--- ex1/index.php
+++ ex1/index.php
@@ -2,8 +2,9 @@
 
 require "vendor/autoload.php";
 
-use app\classes\Pessoa;
+use app\classes\Upload;
 use app\classes\UploadFoto;
+/*use app\classes\Pessoa;
 use app\classes\AtividadePessoa;
 
 $pessoa = new Pessoa("João", 20, "joao@example.com");
@@ -16,3 +17,11 @@ echo $atividade->andar();
 echo PHP_EOL;
 
 $upload = new UploadFoto('foto.jpg');
+*/
+
+/* 
+Os :: são referentes a métodos estáticos. São metódos de classe e não de Objetos.
+Métodos estáticos são acessados diretamente pela classe, sem precisar de um objeto.
+*/
+//UploadFoto::TESTE("Edu");
+
